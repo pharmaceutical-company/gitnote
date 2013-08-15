7832d1ef29d60774351d512709ec711e56b356da
yjroot <yjroot@gmail.com>
2013-08-15 18:19:03 +0900

    Added tool for create new repository.

--- application/tools/git.py
+++ application/tools/git.py
@@ -1,6 +1,8 @@
-import random 
+import os, random 
 
-import dulwich
+from dulwich.repo import Repo
+
+REPO_DIR = '/tmp/gitnote'
 
 def generate_id():
     id = '%05d' % random.randint(1, 99999)
@@ -8,5 +10,12 @@ def generate_id():
 #        return generate_id():
     return id
 
+def new_repo():
+    repo_id = generate_id()
+    repo_path = os.path.join(REPO_DIR, generate_id())
 
+    os.makedirs(repo_path)
+    repo = Repo.init_bare(repo_path)
+
+    return repo_id
 

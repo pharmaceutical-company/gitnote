2f3f210cc7395c0ea5ec56acbdcf635f230b577b
DrugDealer <codeholic.arena@gmail.com>
2013-08-16 21:12:49 +0900

    login-test

---
+++ webserver/user.py
@@ -0,0 +1,35 @@
+import sqlalchemy.orm
+import sqlalchemy.ext.declarative
+
+#: SQLAlchemy session class.
+#:
+#: .. seealso:: SQLAlchemy --- :ref:`session_toplevel`
+Session = sqlalchemy.orm.sessionmaker(bind=engine, autocommit=True)
+
+#: SQLAlchemy declarative base class.
+#:
+#: .. seealso:: SQLAlchemy --- :ref:`declarative_toplevel`
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+
+class User(Base):
+	__tablename__ = 'users'
+	userid = Column(Integer, primary_key=True)
+
+	login = Column(Unicode(45), nullable=False, unique=True)
+
+	password_hash = orm.deferred(Column(String(32), nllable=False))
+
+    name = Column(Unicode(45), nullable=False, index=True)
+
+    created_at = orm.deferred(Column(DateTime(timezone=True), nullable=False,
+                                     default=functions.now(), index=True),
+                              group='profile')
+	def __init__(self, login, password_hash, name):
+		self.login = login
+		self.password_hash = password_hash
+		self.name = name
+	def __repr__(self):
+		return "<User('%s','%s','%s')>" % (self.login, self.password_hash, self.name)
+
+
